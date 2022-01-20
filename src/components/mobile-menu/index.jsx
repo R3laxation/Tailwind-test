436ff5a339ce1665d1bfd53f410b0264280cc43d
R3laxation <memories@bk.ru>
2022-01-20 18:23:30 +0600

    Added second div styles for MobileMenu

--- src/components/mobile-menu/index.jsx
+++ src/components/mobile-menu/index.jsx
@@ -4,7 +4,7 @@ export const MobileMenu = ({isOpen}) => {
     return (
         <>
             <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}></div>
-            <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-height-[150vh]`}></div>
+            <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-height-[150vh] ${isOpen ? 'flex' : 'hidden'}`}></div>
         </>
     );
 };
