ba34ae178ec5fd853cae2eca4312cf5961aa98a9
R3laxation <memories@bk.ru>
2022-03-17 16:43:01 +0600

    Added first div in Mobile-menu component

--- src/components/mobile-menu/index.jsx
+++ src/components/mobile-menu/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const MobileMenu = ({isOpen}) => {
     return (
         <>
-
+            <div className="absolute top-0 left-0 right-0"></div>
         </>
     );
 };
