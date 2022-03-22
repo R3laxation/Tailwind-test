12d3d71f3fcf0ce1bf759f8f6ef74367f1833fe7
R3laxation <memories@bk.ru>
2022-03-22 16:45:46 +0600

    Changed first div styles in MobileMenu

--- src/components/mobile-menu/index.jsx
+++ src/components/mobile-menu/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const MobileMenu = ({isOpen}) => {
     return (
         <>
-            <div className="absolute top-0 left-0 right-0"></div>
+            <div className="absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh]"></div>
         </>
     );
 };
