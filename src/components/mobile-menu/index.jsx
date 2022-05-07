be4beeb5859c3cf560da720554d648741c8b4f68
R3laxation <memories@bk.ru>
2022-05-07 18:31:27 +0600

    Added styles in first div in Nav

--- src/components/mobile-menu/index.jsx
+++ src/components/mobile-menu/index.jsx
@@ -6,7 +6,7 @@ export const MobileMenu = ({isOpen}) => {
             <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}></div>
             <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-height-[150vh] ${isOpen ? 'flex' : 'hidden'}`}>
                 <nav className={'my-20'}>
-                    <div >
+                    <div className={'flex flex-col space-y-5 p-2'}>
 
                     </div>
                 </nav>
