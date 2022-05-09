b35356f5dcf47dde309ede1dc46cd47615e61526
R3laxation <memories@bk.ru>
2022-05-09 18:15:44 +0600

    Added second div in MobileMenu

--- src/components/mobile-menu/index.jsx
+++ src/components/mobile-menu/index.jsx
@@ -4,6 +4,7 @@ export const MobileMenu = ({isOpen}) => {
     return (
         <>
             <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}></div>
+            <div className="absolute right-0 top-0"></div>
         </>
     );
 };
