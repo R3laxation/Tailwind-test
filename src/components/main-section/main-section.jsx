9a4031e9e18ba3e936b4cd00a42e6a1c9124acf3
R3laxation <memories@bk.ru>
2022-11-01 15:33:54 +0600

    Added Meet icon in MainSection

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -17,6 +17,7 @@ export const MainSection = () => {
                 <div className={'flex'}>
                     <Databiz/>
                     <Audiophile/>
+                    <Meet/>
                 </div>
             </div>
         </section>
