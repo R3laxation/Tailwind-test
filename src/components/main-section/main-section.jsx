fc1f0f3598971e53f8eb9785e5963e81060c5578
R3laxation <memories@bk.ru>
2022-11-02 15:17:20 +0600

    Added first column in MainSection

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import MenImage from '../../images/image-hero-desktop.png';
+import MenImageMobile from '../../images/image-hero-mobile.png';
+import { ReactComponent as Databiz } from '../../images/client-databiz.svg';
+import { ReactComponent as Audiophile } from '../../images/client-audiophile.svg';
+import { ReactComponent as Meet } from '../../images/client-meet.svg';
+import { ReactComponent as Maker } from '../../images/client-maker.svg';
 
 export const MainSection = () => {
     return (
-        <div>
-            Main-section
-        </div>
+        <section className={'flex w-full mt-6'}>
+            <div className="relative w-2/4 text-left mt-12">
+                <h1 className={'text-8xl font-black mt-6 whitespace-pre-line'}>{`Make\n remote work`}</h1>
+            </div>
+        </section>
     );
 };
