971d07ec71ff812e7c1ce9250948262857258a7e
R3laxation <memories@bk.ru>
2022-11-02 15:28:32 +0600

    Changed MainSection layout

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -9,12 +9,12 @@ import {Button} from '../button';
 
 export const MainSection = () => {
     return (
-        <section className={'flex w-full mt-6'}>
-            <div className="relative w-2/4 text-left mt-12">
-                <h1 className={'text-8xl font-black mt-60 whitespace-pre-line'}>{`Make\n remote work`}</h1>
-                <p className={'text-medium-gray text-lg mt-12 whitespace-pre-line'}>  {`Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n Amet ullam dolorum animi atque vitae exercitationem\n quibusdam maiores adipisci qui harum.`}</p>
+        <section className={'w-full flex justify-center flex-col mt-6 xl:flex-row'}>
+            <div className={"relative text-center order-2 xl:order-1 xl:text-left xl:w-2/4 xl:mt-60 mt-12"}>
+                <h1 className={'text-3xl xl:text-8xl font-black xl:whitespace-pre-line mt-6'}>{`Make\n remote work`}</h1>
+                <p className={'text-medium-gray text-lg my-12 whitespace-pre-line'}>  {`Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n Amet ullam dolorum animi atque vitae exercitationem\n quibusdam maiores adipisci qui harum.`}</p>
+                <Button isFilled>Learn More</Button>
             </div>
-            <Button/>
         </section>
     );
 };
