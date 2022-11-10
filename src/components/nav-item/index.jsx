39e5ec540717ff332449b54f74989eeb1fad1fae
R3laxation <memories@bk.ru>
2022-11-10 20:49:42 +0600

    Added styles in nav-item

--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const NavItem = ({text = ''}) => {
+
+export const NavItem = ({text = 'asd'}) => {
     return (
         <div className={'relative'}>
             <div className={'flex space-x-2 cursor-pointer'}>
-                <span className={'text-medium-gray'}>{text}</span>
+                <span className={'text-medium-gray hover:text-almost-black'}>{text}</span>
             </div>
-
         </div>
     );
 };
