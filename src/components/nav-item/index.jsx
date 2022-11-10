ab630f6f97ca3c6ec56bb1ac1096bf3b38f0b0cb
R3laxation <memories@bk.ru>
2022-11-10 21:54:04 +0600

    Added callBack with text in nav component

--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as ArrowDownIcon } from '../../images/icon-arrow-down.svg';
 
-export const NavItem = ({text = ''}) => {
+export const NavItem = ({text = '', children}) => {
+
+    const [selected, setSelected] = useState('');
+
     return (
         <div className={'relative'}>
             <div className={'flex space-x-2 cursor-pointer items-center'}>
-                <span className={'text-medium-gray hover:text-almost-black'}>{text}</span>
+                <span className={'text-medium-gray hover:text-almost-black'}
+                      onClick={() => children && setSelected(text !== selected ? text : '')}
+                >
+                    {text}
+                </span>
                 <ArrowDownIcon/>
             </div>
+            {children}
         </div>
     );
 };
