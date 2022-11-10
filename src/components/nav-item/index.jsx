2617c2d947dbb1bd906e7ca2a075d7d30feab9d5
R3laxation <memories@bk.ru>
2022-11-10 20:55:35 +0600

    Added icon in nav

--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {ReactComponent as ArrowDownIcon } from '../../images/icon-arrow-down.svg';
 
 export const NavItem = ({text = 'asd'}) => {
     return (
         <div className={'relative'}>
             <div className={'flex space-x-2 cursor-pointer'}>
                 <span className={'text-medium-gray hover:text-almost-black'}>{text}</span>
+                <ArrowDownIcon/>
             </div>
         </div>
     );
