9d02be1b5715153e8376f3d8b1b8bf282a1fc08f
R3laxation <memories@bk.ru>
2022-11-10 19:45:05 +0600

    Added styles in wrapper

--- src/components/wrapper/index.jsx
+++ src/components/wrapper/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Wrapper = ({children}) => {
     return (
-        <div className={'container mx-auto py-4 px-2 min-h-screen'} >
+        <div className={'mx-auto py-4 px-2 container min-h-screen'} >
             {children}
         </div>
     );
