b8230467d44c14670831cbf4af89e9508736a003
R3laxation <memories@bk.ru>
2022-11-10 20:32:02 +0600

    Changed package.json

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,9 +4,11 @@ import {NavItem} from '../nav-item';
 
 export const Header = () => {
     return (
-        <header>
+        <header className={'flex'}>
             <LogoIcon/>
             <nav>
+                <NavItem/>
+                <NavItem/>
                 <NavItem/>
             </nav>
         </header>
