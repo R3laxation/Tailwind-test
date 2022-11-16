8617a4357ecd8ff648ac9f07ff88bbb1c69e9cdd
R3laxation <memories@bk.ru>
2022-11-16 18:42:16 +0600

    Created Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import {Wrapper} from './components/wrapper';
+
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
+      <Wrapper>
           Hello world!
-      </h1>
+      </Wrapper>
   );
 }
 
---
+++ src/components/wrapper/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Wrapper = () => {
+    return (
+        <div>
+            Wrapper
+        </div>
+    );
+};
+
